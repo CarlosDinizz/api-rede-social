8913f096731607a0ed3305bb51bad11749b946e4
CarlosDinizz <carlosdzalmeida@gmail.com>
2025-01-25 16:14:19 -0300

    first commit

---
+++ src/main/java/com/api/redeSocialApi/RedeSocialApiApplication.java
@@ -0,0 +1,13 @@
+package com.api.redeSocialApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedeSocialApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedeSocialApiApplication.class, args);
+	}
+
+}
