119d0fd210aab8c072983243fcb67e6be0842645
CarlosDinizz <carlosdzalmeida@gmail.com>
2025-01-25 17:51:54 -0300

    Criação da camada service

--- src/main/java/com/api/redeSocialApi/controllers/UserController.java
+++ src/main/java/com/api/redeSocialApi/controllers/UserController.java
@@ -4,6 +4,7 @@ import com.api.redeSocialApi.dtos.UserDTO;
 import com.api.redeSocialApi.domain.User;
 import com.api.redeSocialApi.repositories.UserRepository;
 import com.api.redeSocialApi.ResponseId;
+import com.api.redeSocialApi.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,31 +16,22 @@ import java.util.UUID;
 @RequestMapping("/users")
 public class UserController {
 
-    private UserRepository repository;
+    private UserService userService;
 
     @Autowired
-    public UserController(UserRepository repository){
-        this.repository = repository;
+    public UserController(UserService userService){
+        this.userService = userService;
     }
-
-
+    
     @GetMapping("/{id}")
     public ResponseEntity<UserDTO> findUser(@PathVariable UUID id){
-        Optional<User> userOptional = repository.findById(id);
-
-        if (userOptional.isPresent()){
-            User user = userOptional.get();
-            UserDTO userDTO = new UserDTO(user);
-            return ResponseEntity.ok(userDTO);
-        }
-
-        return ResponseEntity.notFound().build();
+        UserDTO userDTO = userService.findUser(id);
+        return ResponseEntity.ok(userDTO);
     }
 
     @PostMapping
     public ResponseEntity<ResponseId> registerUser(@RequestBody UserDTO userDTO){
-        User user = new User(userDTO);
-        user = repository.save(user);
-        return ResponseEntity.ok(new ResponseId(user.getId()));
+        ResponseId id = userService.registerUser(userDTO);
+        return ResponseEntity.ok(id);
     }
 }
---
+++ src/main/java/com/api/redeSocialApi/services/UserService.java
@@ -0,0 +1,32 @@
+package com.api.redeSocialApi.services;
+
+import com.api.redeSocialApi.ResponseId;
+import com.api.redeSocialApi.domain.User;
+import com.api.redeSocialApi.dtos.UserDTO;
+import com.api.redeSocialApi.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.UUID;
+
+@Service
+public class UserService {
+
+    private UserRepository repository;
+
+    @Autowired
+    public UserService(UserRepository repository){
+        this.repository = repository;
+    }
+
+    public UserDTO findUser(UUID id){
+        User user = repository.findById(id).orElseThrow(() -> new RuntimeException("Not found"));
+        UserDTO userDTO = new UserDTO(user);
+        return userDTO;
+    }
+
+    public ResponseId registerUser(UserDTO userDTO){
+        User user = new User(userDTO);
+        user = repository.save(user);
+        return new ResponseId(user.getId());
+    }
+}
